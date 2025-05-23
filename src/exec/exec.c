b399198221cedb1c23d6519bbcde4e31f5b08f7d
Yekuuun <yekuuun@proton.me>
2025-05-23 16:04:05 +0200

    UPDATE : adding base redirection & piping

--- src/exec/exec.c
+++ src/exec/exec.c
@@ -83,6 +83,102 @@ static int execute_command(ast *node){
 }
 
 /**
+ * Execute pipe command.
+ */
+int execute_pipeline(ast *node) {
+    int pipefd[2];
+    pid_t pid1, pid2;
+
+    if (pipe(pipefd) == -1)
+        return 1;
+
+    pid1 = fork();
+    if (pid1 == 0) {
+        // Child 1: execute left
+        close(pipefd[0]);
+        dup2(pipefd[1], STDOUT_FILENO);
+        close(pipefd[1]);
+        exit(execute_ast(node->left));
+    }
+
+    pid2 = fork();
+    if (pid2 == 0) {
+        // Child 2: execute right
+        close(pipefd[1]);
+        dup2(pipefd[0], STDIN_FILENO);
+        close(pipefd[0]);
+        exit(execute_ast(node->right));
+    }
+
+    close(pipefd[0]);
+    close(pipefd[1]);
+    waitpid(pid1, NULL, 0);
+    waitpid(pid2, NULL, 0);
+
+    return 0;
+}
+
+/**
+ * Execute command with redirection
+ */
+int execute_redirection(ast *node) {
+    int fd;
+    int saved_fd;
+    int result;
+
+    switch (node->type) {
+        case AST_REDIR_IN:
+            fd = open(node->filename, O_RDONLY);
+            if (fd == -1)
+                return 1;
+
+            saved_fd = dup(STDIN_FILENO);
+            dup2(fd, STDIN_FILENO);
+            close(fd);
+            result = execute_ast(node->left);
+            dup2(saved_fd, STDIN_FILENO);
+            close(saved_fd);
+            return result;
+
+        case AST_REDIR_OUT:
+            fd = open(node->filename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+            if (fd == -1)
+                return 1;
+
+            saved_fd = dup(STDOUT_FILENO);
+            dup2(fd, STDOUT_FILENO);
+            close(fd);
+            result = execute_ast(node->left);
+            dup2(saved_fd, STDOUT_FILENO);
+            close(saved_fd);
+            return result;
+
+        case AST_REDIR_APPEND:
+            fd = open(node->filename, O_WRONLY | O_CREAT | O_APPEND, 0644);
+            if (fd == -1) 
+                return 1;
+
+            saved_fd = dup(STDOUT_FILENO);
+            dup2(fd, STDOUT_FILENO);
+            close(fd);
+            result = execute_ast(node->left);
+            dup2(saved_fd, STDOUT_FILENO);
+            close(saved_fd);
+            return result;
+
+        case AST_HEREDOC:
+            fprintf(stderr, "Heredoc execution not implemented\n");
+            return 1;
+
+        default:
+            fprintf(stderr, "Unknown redirection type\n");
+            return 1;
+    }
+}
+
+
+
+/**
  * Main function in charge of execution. 
  * @param node => a ptr to the head of the ast three
  */
@@ -93,6 +189,13 @@ int execute_ast(ast *node){
     switch(node->type){
         case AST_COMMAND:
             return execute_command(node);
+        case AST_PIPELINE:
+            return execute_pipeline(node);
+        case AST_REDIR_IN:
+        case AST_REDIR_OUT:
+        case AST_REDIR_APPEND:
+        case AST_HEREDOC:
+            return execute_redirection(node);
         default:
             return 1;
     }
