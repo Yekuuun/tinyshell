5261a1c73e999261a89acd267ef62c94b407e6dc
Yekuuun <yekuuun@proton.me>
2025-05-28 13:51:26 +0200

    UPDATE : ending cat implementation

--- programs/cat.c
+++ programs/cat.c
@@ -8,9 +8,12 @@
 #include <stdlib.h>
 #include <sys/stat.h>
 #include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 #define EXIT_SUCCESS 0 
 #define EXIT_FAILURE 1
+#define MAX_READ_LEN 4056
 
 //a base struct to handle cat command flags. => inspiration : https://man7.org/linux/man-pages/man1/cat.1.html
 typedef struct cat_flags {
@@ -52,7 +55,39 @@ static int is_valid_path(const char *path){
  * @param flags => struct containing flags.
  */
 static int cat_main(const char *path, pcat_flags flags){
-    //to do.
+    char ch;
+    int lines  = 0;
+    long count = 0;
+    FILE *fileptr = NULL;
+
+    fileptr = fopen(path, "r");
+    if(!fileptr){
+        printf("[!] Error opening file : %s\n", path);
+        return 1;
+    }
+
+    //reading characters.
+    while((ch = fgetc(fileptr)) != EOF){
+        if(ch == '\n'){
+            if(flags->e_flag)
+                printf("$\n");
+
+            if(flags->n_flag)
+                lines++;
+        }
+
+        if(ch == ' ' && flags->n_flag)
+            continue;
+
+        if(!flags->a_flag && count == MAX_READ_LEN)
+            break;
+
+        
+        printf("%c", ch);
+        count++;
+    }
+
+    return 0;
 }
 
 //entry point.
@@ -96,13 +131,16 @@ int main(int argc, char **argv){
         }
     }
 
-    //handling path.
-    if(optind < argc)
+    if (optind >= argc) {
+        printf("[!] No path provided.\n");
         state = 1; goto __CLEANUP;
+    }
 
     const char *path = argv[optind];
-    if(!path || !is_valid_path(path))
+    if(!is_valid_path(path)){
+        printf("[!] Invalid file path.\n");
         state = 1; goto __CLEANUP;
+    }
 
     //calling main process function.
     state = cat_main(path, flags);
